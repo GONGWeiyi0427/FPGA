48e079a13c0e4f051eed7592b9db603b8f3bed50
Samuel <94922608+GONGWeiyi0427@users.noreply.github.com>
2023-03-28 11:49:47 +0200

    Add files via upload

--- MAE_100MHz.vhd
+++ MAE_100MHz.vhd
@@ -38,6 +38,7 @@ entity MAE_100MHz is
      RESET: in std_logic;
      -- Registre DCC
      DCC_BIT: in std_logic;
+     FETCH: in std_logic;
      COM_REG: out std_logic;
      -- TEMPO
      START_TEMPO: out std_logic;
@@ -55,7 +56,7 @@ end MAE_100MHz;
 architecture Behavioral of MAE_100MHz is
 
 signal last: std_logic;
-signal cpt: INTEGER range 51 to 0;
+signal cpt: INTEGER range 0 to 51;
 
 --MAE
 type etat is(S0,S1,S2,S3,S4,S5);
@@ -86,10 +87,11 @@ begin
  process(EP)
  begin
      case (EP) is
-        when S0 => COM_REG <= '0';
+        when S0 => START_TEMPO <= '0'; COM_REG <= '0'; cpt <= 0;
         when S1 => COM_REG <= '1'; cpt <= cpt + 1;
-        when S2 => GO_1 <= '1';
-        when S3 => GO_0 <= '1';
+        when S2 => GO_1 <= '1'; COM_REG <= '0';
+        when S3 => GO_0 <= '1'; COM_REG <= '0';
+        when S4 => GO_1 <= '0'; GO_0 <= '0';
         when S5 => START_TEMPO <= '1'; 
      end case;
  end process;
