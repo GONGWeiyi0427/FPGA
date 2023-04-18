939d08a6ca876f3cd0e9d899d6e1d509f87e3e98
Samuel <94922608+GONGWeiyi0427@users.noreply.github.com>
2023-04-18 10:39:40 +0200

    Add files via upload

--- Generateur_Trames.vhd
+++ Generateur_Trames.vhd
@@ -35,124 +35,125 @@ begin
 		--> Trame Marche Avant du Train d'Adresse i
 	if Interrupteur(7)='1' then
 	
-		Trame_DCC <= "11111111111111111111111"			-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse/numero du train
-					& 	"0"			-- Start Bit
-					&	"01111111"			-- Champ Commande
-					& 	"0"		-- Start Bit
-					& 	"01111110"			-- Champ Contrôle
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000100"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"01111111"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"01111011"		-- Champ Contrôle
+					&       '1'	;			-- Stop Bit
 
 	-- Interrupteur 6 Activé
 		--> Trame Marche Arrière du Train d'Adresse i
 	elsif Interrupteur(6)='1' then
 	
-		Trame_DCC <= "11111111111111111111111"				-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse
-					& 	"0"			-- Start Bit
-					&	"01011111"			-- Champ Commande
-					& 	"0"			-- Start Bit
-					& 	"01011110"			-- Champ Contrôle
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000100"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"01011111"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"01011011"		-- Champ Contrôle
+					&       '1'	;			-- Stop Bit
 
 
 	-- Interrupteur 5 Activé
 		--> Allumage des Phares du Train d'Adresse i
 	elsif Interrupteur(5)='1' then
 	
-		Trame_DCC <= "11111111111111111111111"				-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse
-					& 	"0"				-- Start Bit
-					&	"10011111"			-- Champ Commande
-					& 	"0"			-- Start Bit
-					& 	"10011110"			-- Champ Contrôle
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"10010000"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"10010001"		-- Champ Contrôle
+					&       '1'	;			-- Stop Bit
 
 	-- Interrupteur 4 Activé
 		--> Extinction des Phares du Train d'Adresse i
 	elsif Interrupteur(4)='1' then
 	
-		Trame_DCC <= "11111111111111111111111"				-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse
-					& 	"0"			-- Start Bit
-					&	"10001111"			-- Champ Commande
-					& 	"0"			-- Start Bit
-					& 	"10001110"			-- Champ Contrôle
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"10000000"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"10000001"		-- Champ Contrôle
+					&       '1'	;			-- Stop Bit
 
 	-- Interrupteur 3 Activé
 		--> Activation du Klaxon (Fonction F11) du Train d'Adresse i
 	elsif Interrupteur(3)='1' then
 	
-		Trame_DCC <= "11111111111111111111111"			-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse
-					&	"0"			-- Start Bit
-					&	"10100100"			-- Champ Commande
-					& 	"0"				-- Start Bit
-					& 	"10100101"			-- Champ Contrôle
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"10100100"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"10100101"		-- Champ Contrôle
+					&       '1'	;			-- Stop Bit
 
 	-- Interrupteur 2 Activé
 		--> Réamorçage du Klaxon (Fonction F11) du Train d'Adresse i
 	elsif Interrupteur(2)='1' then
 	
-		Trame_DCC <= "11111111111111111111111"				-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse
-					& 	"0"			-- Start Bit
-					&	"10100000"			-- Champ Commande
-					& 	"0"			-- Start Bit
-					& 	"10100001"			-- Champ Contrôle
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"10100000"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"10100001"		-- Champ Contrôle
+					&       '1'	;			-- Stop Bit
 
 	-- Interrupteur 1 Activé
-		--> Annonce SNCF (Fonction F13) du Train d'Adresse i (active)
+		--> Annonce SNCF (Fonction F13) du Train d'Adresse i
 	elsif Interrupteur(1)='1' then
 	
-		Trame_DCC <= "11111111111111"				-- Préambule
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Adresse
-					& 	"0"			-- Start Bit
-					&	"11011110"			-- Champ Commande (Octet 1)
-					& 	"0"			-- Start Bit
-					&	"00000001"			-- Champ Commande (Octet 2)
-					& 	"0"			-- Start Bit 
-					& 	"11011110"			-- Champ Contrôle Champ Adresse XOR Champ Commande (Octet 1) XOR Champ Commande (Octet 2)
-					& 	"1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"11011110"		-- Champ Commande (Octet 1)
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Commande (Octet 2)
+					& 		'0'		-- Start Bit
+					& 		"11011110"		-- Champ Contrôle
+					& 	    '1';			-- Stop Bit
 
 	-- Interrupteur 0 Activé
-		--> Annonce SNCF (Fonction F13) du Train d'Adresse i (desactive)
+		--> Annonce SNCF (Fonction F13) du Train d'Adresse i
 	elsif Interrupteur(0)='1' then
 	
-		Trame_DCC <= "11111111111111"				-- Préambule
-					&  "0"				-- Start Bit
-					&  "00000001"				-- Champ Adresse
-					&  "0"				-- Start Bit
-					&  "11011110"				-- Champ Commande (Octet 1)
-					&  "0"				-- Start Bit
-					&  "00000000"			-- Champ Commande (Octet 2)
-					&  "0"				-- Start Bit
-					&  "11011111"				-- Champ Contrôle
-					&  "1";			-- Stop Bit
+		Trame_DCC <= 		"11111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000001"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"11011110"		-- Champ Commande (Octet 1)
+					& 		'0'		-- Start Bit
+					&		"00000000"		-- Champ Commande (Octet 2)
+					& 		'0'		-- Start Bit
+					& 		"11011111"		-- Champ Contrôle
+					& 	    '1';			-- Stop Bit
 
 	-- Aucun Interrupteur Activé
 		--> Arrêt du Train d'Adresse i
 	else 
 	
-		Trame_DCC <= "11111111111111111111111"			-- Préambule
-					&  "0"				-- Start Bit
-					&  "00000001"			-- Champ Adresse
-					&  "0"			-- Start Bit
-					&  "01000000"			-- Champ Commande
-					&  "0"		-- Start Bit
-					&  "01000001"				-- Champ Contrôle
-					&  "1";		-- Stop Bit
-end if;	
+		Trame_DCC <= 		"11111111111111111111111"		-- Préambule
+					& 		'0'		-- Start Bit
+					&		"00000100"		-- Champ Adresse
+					& 		'0'		-- Start Bit
+					&		"01100000"		-- Champ Commande
+					& 		'0'		-- Start Bit
+					& 		"01100100"		-- Champ Contrôle
+					& 	     '1';			-- Stop Bit
+	end if;
+	
 end process;
 
 end Behavioral;
